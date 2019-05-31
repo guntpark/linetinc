add14dd6d83848a793380aedcbf71e8030033a8a
guntpark <root@localhost.localdomain>
2019-05-31 17:17:00 +0900

    add header log.c error.c

--- unixprog/error.c
+++ unixprog/error.c
@@ -5,7 +5,10 @@
 static void err_doit(int, const char*, va_list);
 
 char *pname = NULL;
-
+/*
+ * Nonfatal error related to a system call
+ * Print a message and return.
+ */
 void err_ret (const char *fmt, ...)
 {
     va_list ap;
@@ -14,6 +17,10 @@ void err_ret (const char *fmt, ...)
     va_end(ap);
     return;
 }
+/*
+ * Fatal error related to a system call
+ * Print a message and terminate.
+ */
 void err_sys(const char *fmt, ...)
 {
     va_list ap;
@@ -21,6 +28,46 @@ void err_sys(const char *fmt, ...)
     err_doit(1, fmt, ap);
     va_end(ap);
     exit(1);
+}
+
+/*
+ * Fatal error related to a system call
+ * Print a message, dumpcore, and terminate
+ */ 
+void err_dump (const char *fmt, ...)
+{
+    va_list ap;
+    va_start (ap, fmt);
+    err_doit(1, fmt, ap);
+    va_end(ap);
+    abort(); //dump core and terminate
+    exit(1); 
+}
+
+/*
+ * Nonfatal error related to a system call
+ * Print a message and return.
+ */
+void err_msg (const char *fmt, ...)
+{
+    va_list ap;
+    va_start (ap, fmt);
+    err_doit(0, fmt, ap);
+    va_end (ap);
+    return;
+}
+
+/*
+ * Fatal error unrelated to a system call
+ * Print a message, dumpcore, and terminate
+ */ 
+void err_quit (const char *fmt, ...)
+{
+    va_list ap;
+    va_start (ap, fmt);
+    err_doit(0, fmt, ap);
+    va_end(ap);
+    exit(1); 
 }
 
 static void 
@@ -31,12 +78,18 @@ err_doit(int errnoflag, const char *fmt, va_list ap)
 
     errno_save = errno;
     vsprintf(buf, fmt, ap);
+    
     if(errnoflag)
         sprintf(buf+strlen(buf), ": %s", strerror(errno_save));
+    
     strcat(buf, "\n");
+    
     fflush(stdout);
+    
     fputs(buf, stderr);
+    
     fflush(NULL);
+    
     return;
 }
 
--- unixprog/ourhdr.h
+++ unixprog/ourhdr.h
@@ -13,7 +13,86 @@
 
 typedef void Sigfunc(int); //for signal handler
  
+#if defined (SIG_IGN) && !defined(SIG_ERR)
+#define SIG_ERR((Sigfunc *)-1)
+#endif
 
-void err_ret (const char *fmt, ...);
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b> ? (a) : (b))
+
+char *path_alloc(int *);
+int  open_max(void);
+void clr_fl(int, int);
+void set_fl(int, int);
+void pr_exit(int);
+void pr_mask(const char*);
+Sigfunc *signal_intr(int, Sigfunc *);
+
+int     tty_cbreak(int);
+int     tty_raw(int);
+int     tty_reset(int);
+void    tty_atexit(int);
+#ifdef ECHO
+struct termios *tty_termios(void);
+#endif
+
+void    sleep_us(unsigned int);
+ssize_t readn(int, void *, size_t);
+ssize_t writen(int, const void *, size_t);
+int     daemon_init(void);
+
+int s_pipe(int *);
+int recv_fd(int, ssize_t (*func)(int, const void*, size_t));
+int send_fd(int, int);
+int send_err(int, int, const char*);
+int serv_listen(const char*);
+int serv_accept(const char*);
+int cli_conn(const char*);
+int buf_args(char *, int (*func)(int, char**));
+
+int ptym_open(char *);
+int ptys_open(int, char *);
+#ifdef TIOCGWINSZ
+pid_t pty_fork(int *, char *, const struct termious *, const struct winsize *);
+#endif
+
+int lock_reg(int, int, int, off_t, int, off_t);
+
+#define read_lock(fd, offset, whence, len) \
+            lock_reg(fd, F_STLK, F_RDLCK, offset, whence, len)
+#define readw_lock(fd, offset, whence, len) \
+            lock_reg(fd, F_SETLKW, F_RDLCK, offset, whence, len)
+#define write_lock(fd, offset, whence, len) \
+            lock_reg(fd, F_SETLK, F_WRLCK, offset, whence, len)
+#define writew_lock(fd, offset, whence, len) \
+            lock_reg(fd, F_SETLKW, F_WRLCK, offset, whence, len)
+#define un_lock (fd, offset, whence, len) \
+            lock_reg(fd, F_SETLK, F_UNLCK, offset, whence, len)
+
+pid_t lock_test(int, int, off_t, int, off_t);
+
+#define is_readlock(fd, offset, whence, len) \
+            lock_test(fd, F_RDLCK, offset, whence, len)
+#define is_writelock(fd, offset, whence, len) \
+            lock_test(fd, F_WRCLK, offset, whence, len)
+
+/* error.c */
+void err_dump(const char *fmt, ...);
+void err_msg(const char *fmt, ...);
+void err_quit(const char *fmt, ...);
+void err_ret(const char *fmt, ...);
 void err_sys(const char *fmt, ...);
+/* mylog.c */
+void log_msg(const char *fmt, ...);
+void log_open (const char *fmt, int, int);
+void log_quit(const char *fmt, ...);
+void log_ret(const char *fmt, ...);
+void log_sys(const char *fmt, ...);
+
+void TELL_WAIT(void);
+void TELL_PARENT(pid_t);
+void TELL_CHILD(void);
+void WAIT_PARENT(void);        
+void WAIT_CHILD(void);        
+
 #endif /*__ourhdr_h*/
